5f065bc105152a6ff4b92f28aad06e71698bce26
belengg27 <belen01gomez@gmail.com>
2023-11-13 17:15:10 +0100

    math_quiz.py code cleaned

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,92 @@
 import random
 
+def generate_random_integer(minimum: int, maximum: int) -> int:
+    """Generates a random integer between the specified minimum and maximum values.
 
-def function_A(min, max):
+    Parameters
+    ----------
+    minimum : int
+        The minimum value for the random integer.
+    maximum : int
+        The maximum value for the random integer.
+
+    Returns
+    -------
+    int
+        A random integer between the specified minimum and maximum values.
     """
-    Random integer.
+    return random.randint(minimum, maximum)
+
+def choose_operator() -> str:
     """
-    return random.randint(min, max)
+    Chooses a random arithmetic operator from the set {+, -, *}.
 
-
-def function_B():
+    Returns
+    -------
+    str
+        A randomly chosen arithmetic operator.
+    """
     return random.choice(['+', '-', '*'])
 
+def perform_operation(number1, number2, operator: str) -> tuple:
+    """
+    Performs an arithmetic operation on two numbers based on the given operator.
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+    Parameters
+    ----------
+    number1 : int
+        The first operand.
+    number2 : int
+        The second operand.
+    operator : str
+        The arithmetic operator.
 
-def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    Returns
+    -------
+    tuple
+        A tuple containing the formatted operation string and the result of the operation.
+    """
+    operation_str = f"{number1} {operator} {number2}"
+
+    if operator == '+': 
+        result = number1 + number2
+    elif operator == '-': 
+        result = number1 - number2
+    else: 
+        result = number1 * number2
+
+    return operation_str, result
+
+def math_quiz() -> None:
+    """
+    Conducts a math quiz game where the user is presented with math problems and needs to provide correct answers.
+    """
+    score = 0
+    total_questions = 3
 
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(total_questions):
+        number1 = generate_random_integer(1, 10); number2 = generate_random_integer(1, 5); operator = choose_operator()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
-        print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
-        useranswer = int(useranswer)
+        problem_str, solution = perform_operation(number1, number2, operator)
+        print(f"\nQuestion: {problem_str}")
 
-        if useranswer == ANSWER:
+        while True:
+            try:
+                user_answer = int(input("Your answer: "))
+                break
+            except ValueError:
+                print("Error: Please enter a valid number.")
+
+        if user_answer == solution:
             print("Correct! You earned a point.")
-            s += -(-1)
+            score += 1
         else:
-            print(f"Wrong answer. The correct answer is {ANSWER}.")
+            print(f"Wrong answer. The correct answer is {solution}.")
 
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    print(f"\nGame over! Your score is: {score}/{total_questions}")
 
 if __name__ == "__main__":
     math_quiz()
